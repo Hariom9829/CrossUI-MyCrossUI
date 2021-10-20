8bf104b79e3d0cfbcbd03576b9749bc72171e90e
Hariom9829 <90150264+Hariom9829@users.noreply.github.com>
2021-10-20 22:28:13 +0530

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,127 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.MenuBar")
+                .setHost(host,"xui_ui_menubar2")
+                .setItems([
+                    {
+                        "id" : "menu1",
+                        "sub" : [
+                            {
+                                "id" : "normal",
+                                "caption" : "normal"
+                            },
+                            {
+                                "id" : "disabled",
+                                "caption" : "disabled",
+                                "disabled" : true
+                            },
+                            {
+                                "id" : "image",
+                                "caption" : "image",
+                                "imageClass" : "xui-icon-xui"
+                            },
+                            {
+                                "type" : "split"
+                            },
+                            {
+                                "id" : "checkbox 1",
+                                "caption" : "checkbox 1",
+                                "type" : "checkbox"
+                            },
+                            {
+                                "id" : "checkbox 2",
+                                "caption" : "checkbox 2",
+                                "type" : "checkbox"
+                            }
+                        ],
+                        "caption" : "File"
+                    },
+                    {
+                        "id" : "menu2",
+                        "sub" : [
+                            {
+                                "id" : "sub menu 1",
+                                "caption" : "sub menu 1",
+                                "add" : "[Ctrl+F]",
+                                "sub" : [
+                                    {
+                                        "id" : "sub 1",
+                                        "type" : "radiobox"
+                                    },
+                                    {
+                                        "id" : "sub 2",
+                                        "type" : "radiobox"
+                                    },
+                                    {
+                                        "id" : "sub 3"
+                                    }
+                                ]
+                            },
+                            {
+                                "id" : "sub menu 2",
+                                "caption" : "sub menu 2",
+                                "add" : "[Ctrl+T]",
+                                "sub" : [
+                                    "sub 3",
+                                    "sub 4"
+                                ]
+                            }
+                        ],
+                        "caption" : "Edit"
+                    }
+                ])
+                .setTop("6.857142857142857em")
+            );
+            
+            append(
+                xui.create("xui.UI.TreeBar")
+                .setHost(host,"xui_ui_treebar3")
+                .setItems([
+                    {
+                        "id" : "node1",
+                        "sub" : [
+                            "node11",
+                            {
+                                "id" : "node12",
+                                "imageClass" : "xui-icon-xui"
+                            },
+                            "node13",
+                            "node14"
+                        ],
+                        "caption" : "node1"
+                    },
+                    {
+                        "id" : "node2",
+                        "group" : true,
+                        "iniFold" : false,
+                        "caption" : "node2",
+                        "sub" : [
+                            {
+                                "id" : "node21",
+                                "caption" : "node21"
+                            },
+                            {
+                                "id" : "node22",
+                                "caption" : "node22"
+                            },
+                            {
+                                "id" : "node23",
+                                "caption" : "node23"
+                            },
+                            {
+                                "id" : "node24",
+                                "caption" : "node24"
+                            }
+                        ]
+                    }
+                ])
+                .setLeft("0em")
+                .setTop("0em")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
